abd6dec6de3f64ed232b39408c834e163dcbad7e
20161104599 <1416176774@qq.com>
2019-01-04 19:03:41 +0800

    swift12
    Swift

--- llq/ViewController.swift
+++ llq/ViewController.swift
@@ -7,13 +7,66 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UITextField!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    lazy private var progressView: UIProgressView = {
+        self.progressView = UIProgressView.init(frame: CGRect(x: CGFloat(0), y: CGFloat(65), width: UIScreen.main.bounds.width, height: 2))
+        self.progressView.tintColor = UIColor.blue      // 进度条颜色
+        self.progressView.trackTintColor = UIColor.white // 进度条背景色
+        return self.progressView
+    }()
+    let progressview = UIProgressView (frame:CGRect.init(x:0,y:0,width:200,height:10))
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(webView)
+        view.addSubview(progressView)
+        
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        webView.load(URLRequest.init(url: URL.init(string: "https://www.baidu.com/")!))
     }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if keyPath == "estimatedProgress"{
+            progressView.alpha = 1.0
+            progressView.setProgress(Float(webView.estimatedProgress), animated: true)
+            if webView.estimatedProgress >= 1.0 {
+                UIView.animate(withDuration: 0.3, delay: 0.1, options: .curveEaseOut, animations: {
+                    self.progressView.alpha = 0
+                }, completion: { (finish) in
+                    self.progressView.setProgress(0.0, animated: false)
+                })
+            }
+        }
+    }
+    
+    
+
+
+
+    @IBAction func go(_ sender: Any) {
+        let url = result.text
+        webView.load(NSURLRequest(url:NSURL(string:"http://"+url!)!as URL)as URLRequest)
+    }
+    @IBAction func gotoclick(_ sender: Any) {
+        webView.goForward()
+    }
+    @IBAction func gobackclick(_ sender: Any) {
+        webView.goBack()
+    }
+    @IBAction func refreshclick(_ sender: Any) {
+        webView.reload()
+    }
+    
+    
 
 
 }
