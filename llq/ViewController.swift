35e8829ae3c3c06ffe7b9ae21768806539a6c32b
20161104599 <1416176774@qq.com>
2019-01-04 18:11:19 +0800

    Initial Commit

---
+++ llq/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  llq
+//
+//  Created by 16wb395 on 2019/1/4.
+//  Copyright © 2019 16wb395. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
